20f975fc95ac448aa8062b616bf5336516cf97bc
Marc Skov Madsen <masma@orsted.dk>
2021-04-04 20:38:30 +0200

    got the proxy working

--- binder/jupyter-panel-highcharts-server/jupyter_panel_highcharts_server.py
+++ binder/jupyter-panel-highcharts-server/jupyter_panel_highcharts_server.py
@@ -2,7 +2,9 @@
 Function to configure serving the panel example apps via jupyter-server-proxy.
 """
 from glob import glob
+import pathlib
 
+ICON_PATH = str((pathlib.Path(__file__).parent / "panel-highcharts-apps-icon.svg").absolute())
 
 def panel_serve_examples():
     """Returns the jupyter-server-proxy configuration for serving the example notebooks as Panel
@@ -29,6 +31,7 @@ def panel_serve_examples():
         "timeout": 360,
         "launcher_entry": {
             "enabled": True,
-            "title": "Panel HighChart Apps",
+            "title": "Panel Highcharts Apps",
+            "icon_path": ICON_PATH,
         },
     }
--- binder/jupyter-panel-highcharts-server/setup.py
+++ binder/jupyter-panel-highcharts-server/setup.py
@@ -1,14 +1,16 @@
+"""This setup.py will install a package that configures the jupyter-server-proxy to
+panel serve the example notebooks."""
 import setuptools
 
 setuptools.setup(
-  name="jupyter-panel-highcharts-server",
-  # py_modules rather than packages, since we only have 1 file
-  py_modules=['jupyter_panel_highcharts_server'],
-  entry_points={
-      'jupyter_serverproxy_servers': [
-          # name = packagename:function_name
-          'panel = jupyter_panel_highcharts_server:panel_serve_examples',
-      ]
-  },
-  install_requires=['jupyter-server-proxy'],
+    name="jupyter-panel-highcharts-server",
+    # py_modules rather than packages, since we only have 1 file
+    py_modules=["jupyter_panel_highcharts_server"],
+    entry_points={
+        "jupyter_serverproxy_servers": [
+            # name = packagename:function_name
+            "panel = jupyter_panel_highcharts_server:panel_serve_examples",
+        ]
+    },
+    install_requires=["jupyter-server-proxy"],
 )
